5aa25471ede7a42b82583dd8829b1b698158099b
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-06-27 23:38:32 +0100

    Add playlist view

---
+++ src/routes/views/index.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const {
+	renderHomePage,
+	renderLoginPage,
+	renderSignupPage,
+	renderDashboardPage,
+	renderCreatePlaylistPage,
+	renderPlaylistPage,
+} = require("../../controllers/views");
+
+const router = Router();
+
+router.get("/", renderHomePage);
+router.get("/login", renderLoginPage);
+router.get("/signup", renderSignupPage);
+router.get("/dashboard", renderDashboardPage);
+router.get("/create-playlist", renderCreatePlaylistPage);
+router.get("/playlists/:id", renderPlaylistPage);
+
+module.exports = router;
